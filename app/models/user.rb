8d60423c699fcfe60f6ac8d28be6e27f944bf7ee
sneeu-leeu <sa070197@gmail.com>
2022-06-01 18:05:58 +0200

    Add: validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :deals
+
+  validates :name, :amount, presence: true, length: { maximum: 100 }
 end
