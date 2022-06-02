12d26b25ec311bb19e2ea895f1dc73ac474091b5
sneeu-leeu <sa070197@gmail.com>
2022-06-02 11:49:02 +0200

    Add splash controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   authenticated :user do
     root to: 'categories#index', as: :authenticated_root
   end
-  root to: 'splash#index'
-
+  root to: redirect('categories#index')
   devise_for :users
 
   resources :categories
+  get 'welcome', to: 'splash#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_01_165340) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_02_080552) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "catagories", force: :cascade do |t|
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "icon", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "categories_deals", id: false, force: :cascade do |t|
@@ -35,6 +39,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_01_165340) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -46,5 +51,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_01_165340) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "users"
   add_foreign_key "deals", "users", column: "author_id"
 end
