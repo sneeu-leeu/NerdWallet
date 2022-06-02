b5b5003fdae96fd1b629f6b0318c960e333452f7
sneeu-leeu <sa070197@gmail.com>
2022-06-02 14:26:36 +0200

    Fix: linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  # authenticated :user do
-  #   root to: 'categories#index', as: :authenticated_root
-  # end
+  authenticated :user do
+    root to: 'categories#index', as: :authenticated_root
+  end
   root to: 'splash#index'
 
   devise_for :users
