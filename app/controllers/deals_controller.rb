5a22de3f878c38e96511046aa041033954e3aae5
sneeu-leeu <sa070197@gmail.com>
2022-06-02 22:41:33 +0200

    Fix: refactor code

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,9 +1,12 @@
 class DealsController < ApplicationController
   load_and_authorize_resource
 
+  def index
+    redirect_to categories_url
+  end
+
   def new
     @deal = Deal.new
-    set_categories
   end
 
   def create
@@ -14,7 +17,6 @@ class DealsController < ApplicationController
       if @deal.save
         format.html { redirect_to @deal, notice: 'Transaction was successfully added.' }
       else
-        set_categories
         format.html { render :new, status: :unprocessable_entity }
       end
     end
@@ -22,11 +24,7 @@ class DealsController < ApplicationController
 
   private
 
-  def set_categories
-    @categories = current_user.categories.map { |category| [category.name, category.id] }
-  end
-
   def deal_params
-    params.fetch(:deal, {}).permit(:name, :amount)
+    params.fetch(:deal, {}).permit(:name, :amount, :categories)
   end
 end
--- app/models/deal.rb
+++ app/models/deal.rb
@@ -1,6 +1,6 @@
 class Deal < ApplicationRecord
   validates :name, :amount, presence: true, length: { maximum: 100 }
-  validates :amount, numericality: { greater_than: 0 }
+  validates :name, :amount, :categories, presence: true, length: { maximum: 100 }
 
   belongs_to :author, class_name: 'User'
   has_and_belongs_to_many :categories
